e0138993efac0ed5a4cbbc6428d0d3f17e0541ba
indras9990000080002 <111412870+indras9990000080002@users.noreply.github.com>
2022-09-16 13:04:39 +0700

    Update haikal.js

--- haikal.js
+++ haikal.js
@@ -500,6 +500,27 @@ haikal.relayMessage(m.chat, document.message, { messageId: document.key.id })
 }
 break
 //=================================================//
+case 'setppgroup': case 'setppgrup': case 'setppgc': {
+if (isBan) throw sticBanLu(from)
+if (!isRegistered) return replyReg(mess.verif)
+if (!m.isGroup) throw mess.group
+if (!isOwner) throw false
+sticWait(from)
+if (!quoted) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+if (!/image/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+if (/webp/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+let media = await haikal.downloadAndSaveMediaMessage(quoted)
+try {
+if (args[0] == "/full") {
+const { generateProfilePicture } = require("./baseikal/lib/myfunc")
+var { img } = await generateProfilePicture(media)
+await haikal.query({ tag: 'iq',attrs: { to: m.chat, type:'set', xmlns: 'w:profile:picture'}, content: [{ tag: 'picture', attrs: { type: 'image' }, content: img }]})
+} else { await haikal.updateProfilePicture(m.chat, { url: media }) }
+m.reply(mess.success)
+} catch { m.reply('Gagal Mengganti Photo Profile') }
+}
+break
+//=================================================//
 case 'bug4': {
 if (!isCreator) return
 if (isBan) throw sticBanLu(from)
